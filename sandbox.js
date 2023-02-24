6ecc9676edca8d4f6601a4f31c2d4f6eae37c6c9
Kellan Mc Naughton <kellanmcn@gmail.com>
2023-02-24 18:30:48 +0200

    Addded HTML and JS
    Using basic string methods

---
+++ sandbox.js
@@ -0,0 +1,31 @@
+// strings
+console.log('hello, world');
+
+let email = 'kellanmcn@gmail.com';
+console.log(email);
+
+// string concatentation
+
+let firstName = 'Kellan';
+let lastName = 'Mc Naughton';
+
+let fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
+
+// getting characters
+
+console.log(fullName[2]);
+
+// string length
+
+console.log(fullName.length);
+
+// string methods
+
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result, fullName);
+
+let index = email.indexOf('@');
+console.log(index);
